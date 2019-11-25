e2e4880c9ab80d341b0c01782659981906908c4a
matthewscottconroy <matthewscottconroy@gmail.com>
2019-11-25 12:46:18 -0500

    Update FilesSimplified.java

--- examples/FilesSimplified.java
+++ examples/FilesSimplified.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 import java.io.File;
 import java.io.IOException;
 
-public class FilesSimplified throws IOException{
-  public static void main(String [] args){
+public class FilesSimplified {
+  public static void main(String [] args) throws IOException{
     Scanner fileIn = new Scanner(new File("example.txt"));
     
     while(fileIn.hasNext()){
